a6904a4b03e293b3b32609151bc4eb36a1506cfd
askrzypczak <58861577+askrzypczak@users.noreply.github.com>
2021-02-13 15:25:08 -0600

    fix config

--- nuxt.config.js
+++ nuxt.config.js
@@ -29,12 +29,6 @@ export default {
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
 
-  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
-  buildModules: [
-    // https://go.nuxtjs.dev/eslint
-    '@nuxtjs/eslint-module'
-  ],
-
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
     // https://go.nuxtjs.dev/bootstrap
