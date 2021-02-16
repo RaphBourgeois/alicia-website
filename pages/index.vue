09fc79bd5cb60ebe659c3906aa5dcc8d32f72bf4
Raphael Bourgeois <raphal.bourgeois@gmail.com>
2021-02-15 18:02:45 -0600

    firs commit

--- pages/index.vue
+++ pages/index.vue
@@ -1,30 +1,70 @@
 <template>
-  <div class="container">
-    <div>
-      <Logo />
-      <h1 class="title">
-        alicia-website
-      </h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
-    </div>
-  </div>
+  <b-container fluid>
+  <b-navbar toggleable="lg" type="dark" variant="info">
+            <b-navbar-brand href="#">Alicia Queen-Bourgeois Voice Over</b-navbar-brand>
+            <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+            <b-collapse id="nav-collapse" is-nav>
+              <b-navbar-nav>
+                <b-nav-item href="#">Demos</b-nav-item>
+                <b-nav-item href="#">Contact Me</b-nav-item>
+              </b-navbar-nav>
+              <!-- Right aligned nav items -->
+              <b-navbar-nav class="ml-auto">
+              
+              </b-navbar-nav>
+            </b-collapse>
+    </b-navbar>
+
+  <b-row class="test;">
+    <b-col style="text-align: center;"><img src="../assets/background_1.jpg"/></b-col>
+    <b-col>
+      <b-row class="test">
+        <h2 class="demoBox">Commercial Demos</h2>
+      </b-row>
+      <b-row>
+        <b-col>
+          Best way to kill moles!
+        </b-col>
+        <b-col>
+        <audio controls>
+          <source src="https://file-examples-com.github.io/uploads/2017/11/file_example_MP3_5MG.mp3" type="audio/mpeg">
+          Your browser does not support the audio tag.
+        </audio>
+        </b-col>
+      </b-row>
+      <b-row>
+        <b-col>
+          Best way to kill bees!
+        </b-col>
+        <b-col>
+        <audio controls>
+          <source src="https://file-examples-com.github.io/uploads/2017/11/file_example_MP3_5MG.mp3" type="audio/mpeg">
+          Your browser does not support the audio tag.
+        </audio>
+        </b-col>
+      </b-row>
+    </b-col>
+  </b-row>
+  <b-row>
+    <b-col style="text-align: center;"><img src="../assets/background_2.jpg" /></b-col>
+    <b-col>
+        <h2>Animation Demos</h2>
+    </b-col>
+  </b-row>
+  <b-row>
+    <b-col style="text-align: center;"><img src="../assets/background_3.jpg" /></b-col>
+    <b-col>
+        <h3>E-books Demos</h3>
+    </b-col>
+  </b-row>
+    <b-row>
+    <b-col style="text-align: center;"><img src="../assets/background_3.jpg" /></b-col>
+    <b-col>
+        <h3>E-books Demos</h3>
+    </b-col>
+  </b-row>
+</b-container>
 </template>
 
 <script>
@@ -39,6 +79,15 @@ export default {}
   justify-content: center;
   align-items: center;
   text-align: center;
+}
+
+.demoBox {
+  align-v: center;
+}
+
+.test {
+  background-color: rgba(86,61,124,.15);
+  border: 1px solid rgba(86,61,124,.2);
 }
 
 .title {
